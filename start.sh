4cec72da2bae3a325aea98280260d1ecff37e548
wadwdwd1 <imrbk123abc@proton.me>
2025-04-20 11:41:47 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 wget https://download948.mediafire.com/t6p1g4rhxcdgAgHnFMRpiXPA6RiuA6vh8LjDUiGoC_Mgkl1xjYUhVnnJWSDSuHbPzunJmVrKC8aGkY-8gtHpb8fesI0xbIIih3WQc2GP8mtuTvFBGbkkFg5CQjaM3TeDpyWLO_hj4BNtdOLDhtooplJjlvh0crDvjgxYPF3sYfjv5Xw/ysa88nkumda9cg0/Eaglercraft-1.12-Server-Hosting-codespaces-ONLY.zip
 unzip Eaglercraft-1.12-Server-Hosting-codespaces-ONLY.zip
+cd Eaglercraft-1.12-Server-Hosting-codespaces-ONLY
 cd bungee
 java -jar bungee.jar &
 cd ..
